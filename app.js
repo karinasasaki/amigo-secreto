36809f03bfce906fa5eaa0591a346bf3169970d0
Karina Sasaki <karinasasaki99@gmail.com>
2025-01-25 12:13:20 -0400

    feat: Implementa funcionalidade de sorteio

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+function adicionarAmigo() {
+	let amigo = document.querySelector("input").value;
+
+	if (amigo.trim().length === 0) {
+		alert("Por favor, insira um nome.");
+	} else {
+		amigos.push(amigo);
+		limparCampo();
+	}
+
+	exibirAmigos();
+}
+
+function limparCampo() {
+	let campo = document.querySelector("input");
+	campo.value = "";
+}
+
+function exibirAmigos() {
+	let ul = document.getElementById("listaAmigos");
+	ul.innerHTML = "";
+	amigos.forEach((nome) => {
+		ul.innerHTML += `<li>${nome}</li>\n`;
+	})
+}
+
+function sortearAmigo() {
+	if (amigos.length > 0) {
+		let numeroSorteado = Math.floor(Math.random() * amigos.length);
+		let amigoSorteado = amigos[numeroSorteado];
+		let ul = document.getElementById("resultado");
+		ul.innerHTML = `<li>${amigoSorteado}</li>\n`;
+	}
+}
